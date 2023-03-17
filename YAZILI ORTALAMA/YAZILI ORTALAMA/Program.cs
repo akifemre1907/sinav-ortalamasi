540af21ab5c845b7085b9e26efa429568c5a53b7
akifemre1907 <akifemre81@gmail.com>
2023-03-17 19:52:22 +0300

    sınav-not-ortalama

---
+++ YAZILI ORTALAMA/YAZILI ORTALAMA/Program.cs
@@ -0,0 +1,21 @@
+﻿double s1, s2, ort;
+
+Console.Write("1. Sınav notunu giriniz: ");
+s1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. Sınav notunu giriniz: ");
+s2 = Convert.ToInt32(Console.ReadLine());
+
+ort = (s1 + s2) / 2;
+
+Console.WriteLine("-------------------------------------------");
+
+if ( ort >= 50 )
+{
+    Console.WriteLine("Ortalama puanınız " + ort + ". Öğrenci geçti.");
+}
+
+else
+{
+    Console.WriteLine("Ortalama puanınız " + ort + ". Öğrenci kaldı.");
+}
